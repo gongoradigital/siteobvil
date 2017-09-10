736b55d8552539163b28a40157745a2af01ec8c9
glorieux-f <frederic.glorieux@fictif.org>
2017-09-10 12:19:18 +0200

    meilleur doc de la conf

--- _conf.php
+++ _conf.php
@@ -1,12 +1,12 @@
 <?php
 return array(
-  "srcdir" => dirname( __FILE__ ),
-  "destdir" => ".",
-  "cmdup" => "git submodule update --remote 2>&1",
-  "pass" => null, // Mot de passe obligatoire
-  "srcglob" => array( "polemos/*.xml", "gongora_obra-poetica.xml" ), // pour mise à jour de la polémique
+  "title" => 'Góngora', // Nom du corpus
+  "srcdir" => dirname( __FILE__ ), // dossier source depuis lequel exécuter la commande de mise à jour
+  "cmdup" => "git submodule foreach git pull origin gh-pages 2>&1",
+  "pass" => null, // Mot de passe à renseigner à l’installation
+  "srcglob" => array( "polemos/*.xml" ), // pour mise à jour de la polémique
   "sqlite" => "polemos.sqlite", // pour les métadonnées de la polémique
-  "formats" => "article, toc, epub, kindle",
-  "title" => 'Góngora',
+  "destdir" => ".", // dossier de base depuis lequel produire les formats ex: ./article/doc_art.html
+  "formats" => "article, toc, epub, kindle", // formats à générer
 );
 ?>
