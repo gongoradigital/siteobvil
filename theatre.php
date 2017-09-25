9148d242f821f58176be511fdc7a73f565d27574
glorieux-f <frederic.glorieux@fictif.org>
2017-09-25 19:23:00 +0200

    exception pour Comedia Venatoria

--- theatre.php
+++ theatre.php
@@ -49,18 +49,20 @@ if ( $docid ) {
     <div id="main">
     <div id="article">
 <?php
-if ($play) {
-  echo '<h1>'.$play['author']."<br/>".$play['title'].'</h1>';
-  echo '
-<details>
+if ( $play ) {
+  if ( $docid != 'gongora_comediavenatoria' ) {
+    echo '<h1>'.$play['author']."<br/>".$play['title'].'</h1>';
+    echo '
+  <details>
 
-<summary>Gráfico de interlocución <i>(pinche aquí para una descripción más detallada)</i></summary>
-<p>Este gráfico se genera de forma automática a partir de la codificación de la obra. Cada disco representa un personaje y su tamaño es proporcional al número de palabras que pronuncia. Las flechas indican a quien las dirige. La distribución de estos discos se basa en un algoritmo para evitar que se crucen las flechas. Los botones situados debajo del gráfico (♻ para mezclar los discos de forma aleatoria, ► para volver a aplicar el algoritmo), pueden ayudar a diferenciar lo que es arbitrario en la disposición relativa de los personajes y lo que, al contrario, viene determinado por la cantidad de las palabras. Los colores han sido elegidos convencionalmente para facilitar la lectura y resultan de una combinación entre género, edad y estatuto de los personajes. <a href="#tables">Abajo</a> encontrará las bases de datos en las que se basa esta imagen.</p>
+  <summary>Gráfico de interlocución <i>(pinche aquí para una descripción más detallada)</i></summary>
+  <p>Este gráfico se genera de forma automática a partir de la codificación de la obra. Cada disco representa un personaje y su tamaño es proporcional al número de palabras que pronuncia. Las flechas indican a quien las dirige. La distribución de estos discos se basa en un algoritmo para evitar que se crucen las flechas. Los botones situados debajo del gráfico (♻ para mezclar los discos de forma aleatoria, ► para volver a aplicar el algoritmo), pueden ayudar a diferenciar lo que es arbitrario en la disposición relativa de los personajes y lo que, al contrario, viene determinado por la cantidad de las palabras. Los colores han sido elegidos convencionalmente para facilitar la lectura y resultan de una combinación entre género, edad y estatuto de los personajes. <a href="#tables">Abajo</a> encontrará las bases de datos en las que se basa esta imagen.</p>
 
-</details>
-  ';
-  echo Dramagraph_Net::graph( $pdo, $docid );
-  echo '<p> </p>';
+  </details>
+    ';
+    echo Dramagraph_Net::graph( $pdo, $docid );
+    echo '<p> </p>';
+  }
   $qobj->execute( array( $docid, 'article' ) );
   echo  current( $qobj->fetch(PDO::FETCH_ASSOC)) ;
   echo '<section class="page" id="tables"> <p> </p>';
